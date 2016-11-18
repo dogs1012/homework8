36b09ba8d325ea8c9cccab70fd7d52b0110e3256
USAN <dogs1012@naver.com>
2016-11-18 13:48:55 +0900

    코드 수정

--- app/src/main/java/com/example/homework8/MainActivity.java
+++ app/src/main/java/com/example/homework8/MainActivity.java
@@ -5,55 +5,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.*;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText ET,ET2;
+    EditText ET, ET2;
     TextView tv;
-
-
+    Button plus, minus, gop, dvi;
+    int num1, num2;
+    Integer Result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ET = (EditText)findViewById(R.id.editText);
-
-        ET2 = (EditText)findViewById(R.id.editText2);
-
-        tv = (TextView)findViewById(R.id.result);﻿
-
-
+        ET = (EditText) findViewById(R.id.editText);
+        ET2 = (EditText) findViewById(R.id.editText2);
+        plus = (Button) findViewById(R.id.button);
+        minus = (Button) findViewById(R.id.button2);
+        gop = (Button) findViewById(R.id.button3);
+        dvi = (Button) findViewById(R.id.button4);
+        tv = (TextView) findViewById(R.id.Result);
+        View.OnClickListener clisten = new View.OnClickListener() {
+            public void onClick(View v) {
+                num1 = Integer.parseInt(ET.getText().toString());
+                num2 = Integer.parseInt(ET2.getText().toString());
+                switch(v.getId()){
+                    case R.id.button :
+                        Result = num1 + num2;
+                        break;
+                    case R.id.button2 :
+                        Result = num1 - num2;
+                        break;
+                    case R.id.button3 :
+                        Result = num1 * num2;
+                        break;
+                    case R.id.button4 :
+                        Result = num1 / num2;
+                        break;
+                }
+                tv.setText("계산결과 : "+Result);
+            }
+        };
+        plus.setOnClickListener(clisten);
+        minus.setOnClickListener(clisten);
+        gop.setOnClickListener(clisten);
+        minus.setOnClickListener(clisten);
     }
-    public void plus(View v){
-
-        int num1 = Integer.parseInt(ET.getText().toString());
-        int num2 = Integer.parseInt(ET2.getText().toString());
-
-        int result = num1 + num2;
-        tv.setText("답은 :"  + result);
-
-    }
-    public void minus(View v){
-
-        int num1 = Integer.parseInt(ET.getText().toString());
-        int num2 = Integer.parseInt(ET2.getText().toString());
-
-        int result = num1 - num2;
-        tv.setText("답은 : " + result);}
-    public void gop(View v){
-
-        int num1 = Integer.parseInt(ET.getText().toString());
-        int num2 = Integer.parseInt(ET2.getText().toString());
-
-        int result = num1 * num2;
-        tv.setText("답은 : " + result);}
-    public void div(View v){
-
-        int num1 = Integer.parseInt(ET.getText().toString());
-        int num2 = Integer.parseInt(ET2.getText().toString());
-
-        int result = num1 / num2;
-        tv.setText("답은 ,: " + result);}
 }
